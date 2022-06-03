45e21e2309ab11c85e06689f307958a582635a9a
P0ol3yG3ek <reuben.pooley@rvatechsolutions.co.nz>
2022-06-03 13:49:15 +1200

    added signup form

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib import auth
 
-# Create your models here.
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __srt__(self):
+        return "@{}".form(self.username)
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'authentication'
@@ -7,4 +7,5 @@ app_name = 'authentication'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
